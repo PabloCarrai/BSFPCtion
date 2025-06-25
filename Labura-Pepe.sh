1c29154eb93f66afb4664ae43d59c43ba53a5f7e
PabloCarrai <pablohernanjuan@hotmail.com>
2025-06-25 17:05:54 -0300

    AL armar apt.conf se muestra mal. Tengo que ver como corno escapar las

--- Labura-Pepe.sh
+++ Labura-Pepe.sh
@@ -8,3 +8,42 @@ fi
 
 #   Tenes permisos para correr el script
 echo "Corriendo como pepe manda"
+
+VALOR="http_proxy $1"
+VALOR1="https_proxy $1"
+
+CNT="
+http_proxy $1
+https_proxy $1
+HTTP_PROXY $1
+HTTPS_PROXY $1 
+"
+
+#   Uno de los archivos a tocar es este
+FILE="/etc/environment"
+#   Veo si existe
+if [ -e $FILE ]
+   then echo "El archivo existe"
+   echo "$CNT" > $FILE
+else
+   echo "El archivo no existe"
+   touch $FILE
+   echo "$CNT" > $FILE
+fi
+
+FAPT="/etc/apt/apt.conf"
+CNT1="
+Acquire::http::Proxy  $1 ; 
+Acquire::https::Proxy  $1 ; 
+Acquire::ftp::Proxy $1 ;
+"
+
+#   Veo si existe
+if [ -e $FAPT ]
+   then echo "El archivo existe"
+   echo $FAPT
+else
+   echo "El archivo no existe"
+   touch $FAPT
+   echo $CNT1 > $FAPT
+fi
